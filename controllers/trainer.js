381a96aafddd03faf9b1c8e923197f920d3592ce
joshuasuzuki <joshuakai.suzuki@gmail.com>
2020-11-04 01:33:28 -0500

    add edit path

--- controllers/trainer.js
+++ controllers/trainer.js
@@ -13,13 +13,26 @@ router.get('/new', async (req, res) => {
   res.render('trainers/new.ejs');
 });
 
+// EDIT
+router.get('/:id/edit', async (req, res) => {
+    let foundTrainer = await Trainer.findById(req.params.id).populate({
+        path: 'pokemon',
+        options: { sort: { ['name']: 1 } },
+      });
+    
+      res.render('trainers/edit.ejs',{
+        trainer: foundTrainer,
+    });
+});
+
+
 // SHOW
 router.get('/:id', async (req, res) => {
 
   let foundTrainer = await Trainer.findById(req.params.id).populate({
     path: 'pokemon',
     options: { sort: { ['name']: 1 } },
-  });;
+  });
 
   //res.send(foundTrainer);
   res.render('trainers/show.ejs', {
@@ -32,6 +45,12 @@ router.post('/', async (req, res) => {
   console.log(req.body);
   let newTrainer = await Trainer.create(req.body);
   res.redirect(`/trainers/${newTrainer.id}`);
+});
+
+// UPDATE
+router.put('/:id', async (req, res) => {
+    console.log("reached update route");
+    res.redirect('/trainers/');
 });
 
 // DELETE
@@ -50,18 +69,18 @@ router.delete('/:id',(req,res)=>{
 });
 
 // UPDATE
-router.put('/:foodId/ingredients', async (req, res) => {
-  let foundFood = await Food.findByIdAndUpdate(
-    req.params.foodId,
-    {
-      $push: {
-        ingredients: req.body.ingredients,
-      },
-    },
-    { new: true, upsert: true }
-  );
-  console.log(foundFood);
-  res.redirect(`/foods/${foundFood.id}`);
-});
+// router.put('/:foodId/ingredients', async (req, res) => {
+//   let foundFood = await Food.findByIdAndUpdate(
+//     req.params.foodId,
+//     {
+//       $push: {
+//         ingredients: req.body.ingredients,
+//       },
+//     },
+//     { new: true, upsert: true }
+//   );
+//   console.log(foundFood);
+//   res.redirect(`/foods/${foundFood.id}`);
+// });
 
 module.exports = router;
